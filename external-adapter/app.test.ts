b7152e45f0960748eceb9a7196d85d03e0270c3f
Kirill Beresnev <derainberk@gmail.com>
2020-06-10 19:33:57 +0300

    Fix typos

--- external-adapter/app.test.ts
+++ external-adapter/app.test.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
 describe('app skeleton test', () => {
-  it('should works', () => {
+  it('should work', () => {
     app.test = 'testing-test';
     expect(app.test).toEqual('testing-test');
   });
