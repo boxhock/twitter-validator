8e8161bf2264a51c10dde2ea8db4ddf1f880f315
Kirill Beresnev <derainberk@gmail.com>
2020-06-18 15:10:13 +0300

    Finish validator signature service

--- external-adapter/services/ValidatorSignatureService.test.ts
+++ external-adapter/services/ValidatorSignatureService.test.ts
@@ -1,31 +1,71 @@
-import ValidatorSignatureService from './ValidatorSignatureService';
+import ValidatorSignatureService, {
+  InvalidPrivateKeyError,
+} from './ValidatorSignatureService';
 import Web3 from 'web3';
+import config from '../config';
 
 const web3 = new Web3();
 
 describe('ValidatorSignatureService', () => {
   const service = new ValidatorSignatureService();
+  let signatureParameters: any;
 
-  it('should return string', () => {
-    expect(
-      service.sign({
-        domainName: 'test.crypto',
-        domainOwner: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe',
-        domainRecordKey: 'validation.twitter.name',
-        domainRecordValue: 'Apple',
-      }),
-    ).toBeDefined();
-  });
-
-  it('should return recoverable signature', () => {
-    const parameters = {
+  beforeEach(() => {
+    signatureParameters = {
       domainName: 'test.crypto',
       domainOwner: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe',
       domainRecordKey: 'validation.twitter.name',
       domainRecordValue: 'Apple',
     };
-    const signature = service.sign(parameters);
-    const publicAddress = web3.eth.accounts.recover('any message', signature);
-    expect(web3.utils.isAddress(publicAddress)).toBeTruthy();
+  });
+
+  it('should return string', () => {
+    expect(service.sign(signatureParameters)).toBeDefined();
+  });
+
+  it('should return recoverable signature', () => {
+    const signature = service.sign(signatureParameters);
+    const address = web3.eth.accounts.recover('any message', signature);
+    expect(web3.utils.isAddress(address)).toBeTruthy();
+  });
+
+  it('should return expected signature', () => {
+    const expectedSignature =
+      '0x0da02c1fbab1be1f9bcb68db88d6782d9c0e8128c36185b3eee464500c1ac22a73c3bc81b7b77e106ac1fa92695cf08b32faed6477d2994236ac27eeb38d41151c';
+    const signature = service.sign(signatureParameters);
+    expect(signature).toEqual(expectedSignature);
+  });
+
+  it('should recover expected address from signature', () => {
+    const expectedAddress = web3.eth.accounts.privateKeyToAccount(
+      config.VALIDATOR_PRIVATE_KEY,
+    ).address;
+    const signature = service.sign(signatureParameters);
+    const message = [
+      signatureParameters.domainName,
+      signatureParameters.domainOwner,
+      signatureParameters.domainRecordKey,
+      signatureParameters.domainRecordValue,
+    ]
+      .map((value) => web3.utils.keccak256(value))
+      .reduce((message, hashedValue) => message + hashedValue, '');
+    const recoveredAddress = web3.eth.accounts.recover(message, signature);
+    expect(recoveredAddress).toEqual(expectedAddress);
+  });
+
+  it('should throw error if invalid private key was set', () => {
+    try {
+      new ValidatorSignatureService('ssss');
+      fail('Should fail with invalid private key');
+    } catch (e) {
+      expect(e instanceof InvalidPrivateKeyError).toBeTruthy();
+    }
+
+    try {
+      new ValidatorSignatureService('');
+      fail('Should fail with invalid private key');
+    } catch (e) {
+      expect(e instanceof InvalidPrivateKeyError).toBeTruthy();
+    }
   });
 });
--- external-adapter/services/ValidatorSignatureService.ts
+++ external-adapter/services/ValidatorSignatureService.ts
@@ -3,19 +3,29 @@ import config from '../config';
 
 const web3 = new Web3();
 
-type SignParameters = {
+type SignatureParameters = {
   domainName: string;
   domainOwner: string;
   domainRecordKey: string;
   domainRecordValue: string;
 };
 
+export class InvalidPrivateKeyError extends Error {}
+
 export default class ValidatorSignatureService {
   constructor(
     private readonly privateKey: string = config.VALIDATOR_PRIVATE_KEY,
-  ) {}
+  ) {
+    try {
+      web3.eth.accounts.privateKeyToAccount(this.privateKey);
+    } catch (e) {
+      throw new InvalidPrivateKeyError(
+        'Invalid private key was set. Check VALIDATOR_PRIVATE_KEY env variable',
+      );
+    }
+  }
 
-  sign(parameters: SignParameters): string {
+  sign(parameters: SignatureParameters): string {
     const messageToSign = [
       parameters.domainName,
       parameters.domainOwner,
@@ -24,9 +34,7 @@ export default class ValidatorSignatureService {
     ]
       .map((value) => web3.utils.keccak256(value))
       .reduce((message, hashedValue) => message + hashedValue, '');
-
     const signResult = web3.eth.accounts.sign(messageToSign, this.privateKey);
-
     return signResult.signature;
   }
 }
